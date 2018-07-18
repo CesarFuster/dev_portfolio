a9d53cc73a9c00695d0b836694ca912f0cbf57ca
cesarfuster <cfuster@uol.com.br>
2018-07-18 16:31:52 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // Importando o React
 import React, { Component } from 'react';
+// Importando o Component Header
+import Header from './components/header/header'
 
 class App extends Component {
   render() {
